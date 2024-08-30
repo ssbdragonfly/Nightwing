0510290346a6c8121fc60039e82076aa0a8a2f28
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-08-30 11:14:18 -0400

    Add quiz app

--- nightwing/settings/__init__.py
+++ nightwing/settings/__init__.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nightwing.apps.auth',
     'nightwing.apps.dashboard',
+    'nightwing.apps.quiz',
 ]
 
 MIDDLEWARE = [

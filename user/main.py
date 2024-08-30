8f570ad4b7016084998a85365fd9088cd7b68540
JacobPercy <jacob.percy@icloud.com>
2024-08-30 15:36:37 -0400

    New page opens for user side

--- user/main.py
+++ user/main.py
@@ -12,14 +12,12 @@ T = TypeVar("T")
 
 def pcall(func: Callable[P, object]) -> Callable[P, None]:
     """Call a function, suppressing all errors."""
-
     @functools.wraps(func)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> None:
         try:
             func(*args, **kwargs)
         except Exception:  # noqa: BLE001
             traceback.print_exc()
-
     return wrapper
 
 
@@ -29,10 +27,23 @@ class App(ctk.CTk):
         self.title("NightWing")
         self.width_1 = self.winfo_screenwidth()
         self.height_1 = self.winfo_screenheight()
+        self.name = ""
         self.geometry("300x" + str(self.height_1 - 75) + "+" + str(self.width_1 - 310) + "+0")
         self.title_gen()
 
-        self.entry = ctk.CTkEntry(
+        self.user_entry = ctk.CTkEntry(
+            master=self,
+            placeholder_text="Enter Name",
+            width=int(0.2 * self.width_1),
+            height=int(0.04 * self.height_1),
+            border_width=5,
+            corner_radius=10,
+            font=("Calibri", int(0.03 * self.height_1)),
+        )
+        self.user_entry.place(relx=0.5, rely=0.3, anchor=ctk.CENTER)
+        self.user_entry.bind("<Return>", lambda event: self.send_code(self.code_entry.get(), self.user_entry.get()))
+
+        self.code_entry = ctk.CTkEntry(
             master=self,
             placeholder_text="Enter Code",
             width=int(0.2 * self.width_1),
@@ -41,15 +52,24 @@ class App(ctk.CTk):
             corner_radius=10,
             font=("Calibri", int(0.03 * self.height_1)),
         )
-        self.entry.place(relx=0.5, rely=0.2, anchor=ctk.CENTER)
-        self.entry.bind("<Return>", lambda event: self.send_code(self.entry.get()))
+        self.code_entry.place(relx=0.5, rely=0.2, anchor=ctk.CENTER)
+        self.code_entry.bind("<Return>", lambda event: self.send_code(self.code_entry.get(), self.user_entry.get()))
 
     @pcall
-    def send_code(self, code: str):
-        response = requests.post(f"http://127.0.0.1:8000/quiz/quiz/{code}")
+    def send_code(self, code: str, name: str):
+        response = requests.post(f"http://127.0.0.1:8000/quiz/quiz/{code}") #where to post responses
         jsonified = response.json()
         self.quiz_id = jsonified["quiz_id"]
+        self.name = name
+        self.render_waiting_screen()
+
+
+    def render_waiting_screen(self):
         self.clear_screen()
+        self.title_gen()
+        self.rendered_name = ctk.CTkLabel(self, text=self.name, font=("Calibri", int(0.02 * self.height_1)))
+        self.rendered_name.place(relx=0.5, rely=0.125, anchor=ctk.CENTER)
+
 
     def clear_screen(self):
         for widget in self.winfo_children():
@@ -59,7 +79,8 @@ class App(ctk.CTk):
         self.title = ctk.CTkLabel(
             self, text="NightWing Quizzes", font=("Calibri", int(0.05 * self.height_1))
         )
-        self.title.pack()
+        self.title.place(relx=0.5, rely=0.075, anchor=ctk.CENTER)
+
 
     def launch_function(self):
         print("test")

50dd4b5c6862b738ce19b772bed4aa042302356f
JacobPercy <jacob.percy@icloud.com>
2024-08-30 14:29:35 -0400

    First screen for user is done

--- user/main.py
+++ user/main.py
@@ -12,20 +12,21 @@ class App(ctk.CTk):
         )
         self.title_gen()
 
-        self.input_text = ctk.StringVar()
         self.entry = ctk.CTkEntry(master=self,
                                placeholder_text="Enter Code",
-                               textvariable=self.input_text,
-                               width=int(0.75 * self.width_1),
-                               height=int(0.2 * self.height_1),
-                               border_width=10,
+                               width=int(0.2 * self.width_1),
+                               height=int(0.04 * self.height_1),
+                               border_width=5,
                                corner_radius=10,
                                font=("Calibri",int(0.03 * self.height_1)))
         self.entry.place(relx=0.5, rely=0.2, anchor=ctk.CENTER)
         self.entry.bind("<Return>", lambda event: self.send_code(self.entry))
 
     def send_code(self, code):
+        #Placeholder for actual talking to website
         print(code.get())
+        self.clear_screen()
+
 
     def clear_screen(self):
         for widget in self.winfo_children():
